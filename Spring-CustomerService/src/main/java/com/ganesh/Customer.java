59df5d45bddf6851fa71d41a12358b65a558d77d
Ganesh <ganeshgarje716@gmail.com>
2026-07-15 15:33:29 +0530

    Create Customer Class

---
+++ Spring-CustomerService/src/main/java/com/ganesh/Customer.java
@@ -0,0 +1,23 @@
+package com.ganesh;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+	
+	
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private long mobileNo;
+	
+	private String address;
+	
+
+}
