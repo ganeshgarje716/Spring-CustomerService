be712e7be1da5c612d9aef84eb28a0e5e09bdd17
Ganesh <ganeshgarje716@gmail.com>
2026-07-15 16:57:30 +0530

    Add @EnableFeignClient Annotation

--- Spring-CustomerService/src/main/java/com/ganesh/SpringCustomerServiceApplication.java
+++ Spring-CustomerService/src/main/java/com/ganesh/SpringCustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.ganesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringCustomerServiceApplication {
 
 	public static void main(String[] args) {
