68ff3f27ff5497163c6e21d07f97c6bc49490c1a
Ganesh <ganeshgarje716@gmail.com>
2026-07-15 16:58:07 +0530

    Add OrderClient Variable

--- Spring-CustomerService/src/main/java/com/ganesh/rest/CustomerRestController.java
+++ Spring-CustomerService/src/main/java/com/ganesh/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.ganesh.rest;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,35 +11,49 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ganesh.Customer;
+import com.ganesh.OrderClient;
 
 @RestController
 public class CustomerRestController {
 	
 	
+	@Autowired
+	OrderClient orderClient;
+	
 	
 	@GetMapping("/customer/{id}")
-	public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
+	public ResponseEntity<Map<String, Object>> getCustomerById(@PathVariable Integer id) {
 		
-		Customer customer = new Customer(id, "Ganesh", "ganesh@gmail.com", 8857013765l, "Pune");
+		Map<String, Object> customer = new LinkedHashMap<>();
 		
-		return new ResponseEntity<Customer>(customer,HttpStatus.OK); 
+		Map<String,Object> order = orderClient.getOrder(id);
+		
+		customer.put("Id", id);
+		customer.put("Name", "Ganesh");
+		customer.put("Address", "Pune");
+		customer.put("Mobile No", 8857013765l);
+		customer.put("Email", "ganesh@gmail.com");
+		customer.put("Order", order);  
+		
+		return new ResponseEntity<Map<String, Object>>(customer,HttpStatus.OK); 
 	}
 	
 	
 	
 	
+	
+	
+	
+	
 //	@GetMapping("/customer/{id}")
-//	public ResponseEntity<Map<String, Object>> getCustomerById(@PathVariable Integer id) {
+//	public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
 //		
-//		Map<String, Object> customer = new LinkedHashMap<>();
+//		Customer customer = new Customer(id, "Ganesh", "ganesh@gmail.com", 8857013765l, "Pune");
 //		
-//		customer.put("Id", id);
-//		customer.put("Name", "Ganesh");
-//		customer.put("Address", "Pune");
-//		customer.put("Mobile No", 8857013765l);
-//		customer.put("Email", "ganesh@gmail.com");
-//		
-//		return new ResponseEntity<Map<String, Object>>(customer,HttpStatus.OK); 
+//		return new ResponseEntity<Customer>(customer,HttpStatus.OK); 
 //	}
+	
+	
+	
 
 }
